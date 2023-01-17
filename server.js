eafd530e8171b1c752f114bdf6ccf955251238f4
Elena Swiderskaya <olenyonock7@rambler.ru>
2023-01-17 22:50:58 +0300

    promise in read html file

--- server.js
+++ server.js
@@ -13,37 +13,48 @@
 // })
 
 //**
-    // SIMPLE HTTP SERVER
-  // const http = require('http')
-  // const fs = require('fs') //file-system, for reading html pages
+// SIMPLE HTTP SERVER
+// const http = require('http')
+// const fs = require('fs') //file-system, for reading html pages
 
-  // ES module - set "type": "module" in the package.json 
-  import { createServer } from 'http'
-  import fs from 'fs' //file-system, for reading html pages
+// ES module - set "type": "module" in the package.json 
+import { createServer } from 'http'
+import fs from 'fs' //file-system, for reading html pages
 
-  const server = createServer((req, res)=>{
-
-    const sendResponse =  (err, data) =>{
-      if(err) {
-        res.write("500: File could not be ridden")
-      } else {
-        res.write(data)
-      }
-       res.end()
-    }
-
-    switch (req.url){
-      case "/":
-         fs.readFile('pages/index.html', sendResponse)
-        break;
-      case "/contacts":
-        fs.readFile('pages/contacts.html', sendResponse)
-        break;
-      default:
-        res.write("404: Not found dinosaurs")
-        res.end()
-        break;
-    }
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
-  server.listen(3500, ()=> console.log("App listening on port 3500"))
+}
+
+const server = createServer(async (req, res) => {
+
+  async function sendResponse(path) {
+    try {
+      let data = await readFile(path);
+      res.write(data)
+      res.end()
+    } catch (error) {
+      res.write("500: File is not be ridden")
+      res.end()
+    }
+  }
+
+  switch (req.url) {
+    case "/":
+      sendResponse('pages/index.html')
+      break;
+    case "/contacts":
+      sendResponse('pages/contacts.html')
+      break;
+    default:
+      res.write("404: Not found dinosaurs")
+      res.end()
+      break;
+  }
+})
+server.listen(3500, () => console.log("App listening on port 3500"))
 //*/

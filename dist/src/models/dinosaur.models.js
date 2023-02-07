ffb0ffb693ee1f19cdc84eee2d3ce0b53a18ebcc
Elena Swiderskaya <olenyonock7@rambler.ru>
2023-02-07 19:48:17 +0300

    remove js temp files

--- dist/src/models/dinosaur.models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Period = void 0;
-var Period;
-(function (Period) {
-    Period["Jurassic"] = "Jurassic";
-    Period["Cretaceous"] = "Cretaceous";
-    Period["Triassic"] = "Triassic";
-})(Period = exports.Period || (exports.Period = {}));
